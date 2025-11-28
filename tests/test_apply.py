ff3613dbdda05019d2f46c4d9592df3375798e2c
pokharkarswayam <vivek.pokharkar@gmail.com>
2025-11-28 14:49:59 +0530

    Test_Apply chnages merged

--- tests/test_apply.py
+++ tests/test_apply.py
@@ -5,11 +5,7 @@ from utils.logger import get_logger
 
 
 class Test_Apply:
-    git
-    push
-    origin
-    main
-    def test_job_apply(self,browser):
+        def test_job_apply(self,browser):
         obj_jobfeed = Jobfeed(browser)
         obj_jobfeed.select_checkboxes()
         obj_jobfeed.click_apply_btn()
