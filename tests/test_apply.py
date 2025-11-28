d1f185438f2749d869e3835eab88709b7c6f5694
pokharkarswayam <vivek.pokharkar@gmail.com>
2025-11-28 14:43:01 +0530

    Updated test_apply

--- tests/test_apply.py
+++ tests/test_apply.py
@@ -5,7 +5,10 @@ from utils.logger import get_logger
 
 
 class Test_Apply:
-
+    git
+    push
+    origin
+    main
     def test_job_apply(self,browser):
         obj_jobfeed = Jobfeed(browser)
         obj_jobfeed.select_checkboxes()
